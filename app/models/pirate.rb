e4732322217179aab0189e5df49db25ae19b5054
Andrea Lovewell <andy.lovewell@gmail.com>
2019-05-01 04:49:34 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  @@all =[]
+  
+  def intialize (name, weight, height)
+    @name =name
+    @weight = weight
+    @height = height
+  end
+
+  def self.Pirate
+    @@all << self
+  end
+  
 end
